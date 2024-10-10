e40c06206c987d7c221fbe1519b8fc090c95ce01
Oleg Molkov <ol.molkov@mail.ru>
2024-10-10 18:32:20 +0800

    Make desription

--- src/GitTest/ArraysTest.java
+++ src/GitTest/ArraysTest.java
@@ -6,6 +6,7 @@ public class ArraysTest {
     public static void main(String[] args) {
         int[] array = {1, 23, 43, 2, 6, 10};
 
+        System.out.println("Вывод массива:");
         for (int i : array) {
             System.out.print(i + " ");
         }

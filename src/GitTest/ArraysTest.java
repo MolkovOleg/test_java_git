757ae82385e0bc45f7da7ca0dffb774580521d64
Oleg Molkov <ol.molkov@mail.ru>
2024-10-10 18:23:44 +0800

    Make sout array

---
+++ src/GitTest/ArraysTest.java
@@ -0,0 +1,12 @@
+package GitTest;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        int [] array = {1, 23, 43, 2, 6, 10};
+
+        System.out.println(Arrays.toString(array));
+    }
+
+}

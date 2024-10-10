5d009f270c455d0c8860a2967318bc6e0b7987bc
Oleg Molkov <ol.molkov@mail.ru>
2024-10-10 16:33:12 +0800

    Third commit

--- src/GitTest/GitTest.java
+++ src/GitTest/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("There is nothing to read here!");
         System.out.println("Just learn Git");
+        System.out.println("Keep walking away!");
     }
 }

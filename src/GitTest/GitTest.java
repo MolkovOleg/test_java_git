bb7df7a601c1812578e4bd490b59c9189df31fc0
Oleg Molkov <ol.molkov@mail.ru>
2024-10-10 16:32:29 +0800

    Second commit

--- src/GitTest/GitTest.java
+++ src/GitTest/GitTest.java
@@ -3,5 +3,6 @@ package GitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("There is nothing to read here!");
+        System.out.println("Just learn Git");
     }
 }
